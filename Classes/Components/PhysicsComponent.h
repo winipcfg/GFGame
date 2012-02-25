ce830be3d0e1a1b8e2739143cd3c5eea67af9c1c
Summoner <winipcfg@gmail.com>
2012-02-26 04:10:08 +0800

    Generic components

---
+++ Classes/Components/PhysicsComponent.h
@@ -0,0 +1,69 @@
+//Copyright (C) 2012 by Gavin Fong
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+
+#ifndef WARRIOR_PHYSICS_COMPONENT_H_
+#define WARRIOR_PHYSICS_COMPONENT_H_
+
+#include <string>
+#include <hash_map>
+#include <Cistron/Cistron.h>
+#include <Box2D/Box2D.h>
+#include <Warrior/TypeDef.h>
+
+namespace Warrior 
+{
+
+/// Stores missile information
+class PhysicsComponent : public Cistron::Component
+{
+public:
+    /// Constructor.
+    PhysicsComponent();
+
+    /// Destructor.
+    ~PhysicsComponent();
+
+    /// Gets the physics body.
+    b2Body* Body()                                          { return bodies_[0]; }
+
+    /// Assign physics body to the unit.
+    /// @param body
+    void AddBody(std::string name, b2Body* body)            { bodies_[name] = body; }
+
+    /// Gets the bounding region.
+    BPolygon GetBoundingRegion() const;
+                
+private:    
+    std::hash_map<std::string, b2Body*>                     bodies_;
+};  
+
+    
+inline PhysicsComponent::PhysicsComponent()
+    : Cistron::Component("PhysicsComponent")
+{
+}
+
+inline PhysicsComponent::~PhysicsComponent()
+{
+}
+    
+} // namespace
+
+#endif // WARRIOR_PHYSICS_COMPONENT_H_
